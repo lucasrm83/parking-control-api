6ed2eb9fb5081d39e42c998e276415e2930ba72b
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-04-05 11:42:04 -0300

    Implementando carController e metodos de requisição de car

---
+++ src/main/java/com/api/parkingcontrol/controllers/CarController.java
@@ -0,0 +1,18 @@
+package com.api.parkingcontrol.controllers;
+
+import com.api.parkingcontrol.services.CarService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cars")
+public class CarController {
+
+    final CarService carService;
+
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+}
--- src/main/java/com/api/parkingcontrol/services/CarService.java
+++ src/main/java/com/api/parkingcontrol/services/CarService.java
@@ -1,9 +1,12 @@
 package com.api.parkingcontrol.services;
 
 
+import com.api.parkingcontrol.entities.Car;
 import com.api.parkingcontrol.repositories.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CarService {
@@ -13,6 +16,10 @@ public class CarService {
 
     public boolean existByLicensePlateCar(String licensePlateCar) {
         return carRepository.existsByLicensePlateCar(licensePlateCar);
+    }
+
+    public Optional<Car> findByBrandCar(String brandCar){
+        return carRepository.findBy()
     }
 
 }
